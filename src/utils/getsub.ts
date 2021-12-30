038cf319cb1d26823df159b42cd77a272a954c7a
Richard Germaine <54249860+rlgerma92@users.noreply.github.com>
2021-12-29 19:52:15 -0700

    minor styling changes in feed

--- src/utils/getsub.ts
+++ src/utils/getsub.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function getSub(sub: string): Promise<any> {
   try {
-    const req = await fetch(`https://www.reddit.com/r/${sub}/.json?jsonp=`);
+    const req = await fetch(`https://www.reddit.com/r/${sub}/hot/.json?limit=100?`);
     if (req.ok) return req.json();
     else throw new Error(req.statusText);
   } catch (error) {
