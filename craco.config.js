26ac021bbd184b04f2cb22a448ee39d33b504df3
Richard Germaine <rlgerma92@outlook.com>
2024-10-09 16:24:29 -0600

    Update ESLint configuration and refactor craco.config.js for improved module exports

--- craco.config.js
+++ craco.config.js
@@ -1,28 +1,30 @@
-import CracoLessPlugin from "craco-less";
+const CracoLessPlugin = require("craco-less");
 
-export const plugins = [
-  {
-    plugin: CracoLessPlugin,
-    options: {
-      lessLoaderOptions: {
-        lessOptions: {
-          modifyVars: {
-            "@primary-color": "#FE4600",
-            "@link-color": "#7A9199",
-            "@success-color": "#52c41a",
-            "@warning-color": "#faad14",
-            "@error-color": "#f5222d",
-            "@font-size-base": "14px",
-            "@heading-color": "rgba(0, 0, 0, 0.85)",
-            "@text-color": "rgba(0, 0, 0, 0.65)",
-            "@text-color-secondary": "rgba(0, 0, 0, 0.45)",
-            "@disabled-color": "rgba(0, 0, 0, 0.25)",
-            "@border-radius-base": "2px",
-            "@border-color-base": "#0D3543",
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#FE4600",
+              "@link-color": "#7A9199",
+              "@success-color": "#52c41a",
+              "@warning-color": "#faad14",
+              "@error-color": "#f5222d",
+              "@font-size-base": "14px",
+              "@heading-color": "rgba(0, 0, 0, 0.85)",
+              "@text-color": "rgba(0, 0, 0, 0.65)",
+              "@text-color-secondary": "rgba(0, 0, 0, 0.45)",
+              "@disabled-color": "rgba(0, 0, 0, 0.25)",
+              "@border-radius-base": "2px",
+              "@border-color-base": "#0D3543",
+            },
+            javascriptEnabled: true,
           },
-          javascriptEnabled: true,
         },
       },
     },
-  },
-];
+  ],
+};
